db06b7326d80495e1a7c209e8a4a1fd7874a8955
Dineshj123 <Dineshj123@users.noreply.github.com>
2015-06-22 23:08:43 +0530

    updated

--- spidertask2.js
+++ spidertask2.js
@@ -1,21 +1,25 @@
 src="http://maps.googleapis.com/maps/api/js";
 var country,state,city,pinCode,latitude,longitude;
 function func(){return;}
-function createCORSRequest(method, url) {
+function createCORSRequest(method, url) 
+{
   var xhr = new XMLHttpRequest();
   if ("withCredentials" in xhr) {
     // XHR for Chrome/Firefox/Opera/Safari.
     xhr.open(method, url, true);
 
-  } else if (typeof XDomainRequest != "undefined") {
+  } 
+  else if (typeof XDomainRequest != "undefined") 
+  {
     // XDomainRequest for IE.
     xhr = new XDomainRequest();
     xhr.open(method, url);
 
-  } else {
+  } 
+  else {
     // CORS not supported.
     xhr = null;
-  }
+       }
   return xhr;
 }
 function initialize()
@@ -25,17 +29,19 @@ if(!latitude&&!longitude){
 latitude=13.0827;
 longitude=80.2707;
 }
-var mapProp = {
+var mapProp = 
+  {
   center:myCenter,
   zoom:5,
   mapTypeId:google.maps.MapTypeId.ROADMAP
   };
-		var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 var marker=new google.maps.Marker({
   position:myCenter,
   draggable:true,
   });
-  google.maps.event.addListener(marker, 'dragend', function(evt){
+  google.maps.event.addListener(marker, 'dragend', function(evt)
+  {
 latitude= evt.latLng.lat().toFixed(3);
 longitude= evt.latLng.lng().toFixed(3);
 if(latitude&&longitude){
@@ -44,14 +50,14 @@ var url="http://maps.googleapis.com/maps/api/geocode/json?latlng="+
         latitude+","+longitude+"&sensor=true";
 		//alert(url);
     var xhr = createCORSRequest('POST', url);
-           if (!xhr) {
+           if (!xhr) 
+           {
              alert('CORS not supported');
            }
          
-           xhr.onload = function() {
-        
+           xhr.onload = function() 
+           {
             var data =JSON.parse(xhr.responseText);
-            
             if(data.results.length>0)
             {
             var locationDetails=data.results[0].formatted_address;
@@ -70,27 +76,27 @@ document.getElementById("box1").value=longitude;
 document.getElementById("cityname").value=city;
  var url="http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude;
  var xmlhttp;
-if (window.XMLHttpRequest)
-  {
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-		//alert(xmlhttp.responseText);
-		var json=JSON.parse(xmlhttp.responseText);
-	//	alert(json['main']['temp']);
-	    var infowindow = new google.maps.InfoWindow({
+               if (window.XMLHttpRequest)
+               {
+               xmlhttp=new XMLHttpRequest();
+               }
+               else
+               {
+               xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+               }
+                       xmlhttp.onreadystatechange=function()
+                       {
+                            if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                               {
+		               //alert(xmlhttp.responseText);
+		                var json=JSON.parse(xmlhttp.responseText);
+	                                                //	alert(json['main']['temp']);
+	                       var infowindow = new google.maps.InfoWindow({
   content:"city"+city+"<br>current temperature in this region:"+json['main']['temp']+"&deg;F<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;currently:"+json['weather'][0]['main']
 });
 infowindow.open(map,marker);
-	}
-}
+	                       }
+                        }                     
 xmlhttp.open("POST",url,true);
 xmlhttp.send();
               document.getElementById("current").innerHTML=country+
@@ -103,7 +109,8 @@ xmlhttp.send();
             
            };
 
-           xhr.onerror = function() {
+           xhr.onerror = function() 
+           {
                alert('Woops, there was an error making the request.<br>There may internet connection problem!!!');
                
            };
